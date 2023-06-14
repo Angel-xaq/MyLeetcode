cd06132bdcc7049bf6fb9a9cbae6f2129b449ea4
Angel <1210772173@qq.com>
2023-06-14 13:24:17 +0800

    try

--- 数组/二维数组的花式遍历/54.螺旋矩阵.js
+++ 数组/二维数组的花式遍历/54.螺旋矩阵.js
@@ -10,7 +10,36 @@
  * @return {number[]}
  */
 var spiralOrder = function(matrix) {
-    let a=0;
+    if(matrix.length==0) return[]; //顾全思想
+    let top=left=0,bottom=matrix.length-1,right=matrix[0].length-1;
+    const result=[];
+    while(top<bottom && left<right){  //关键，当无法形成环的时候，停止迭代
+        for(let i=left;i<right;i++)
+            result.push(matrix[top][i]);
+        for(let i=top;i<bottom;i++)
+            result.push(matrix[i][right]);
+        for(let i=right;i>left;i--)
+            result.push(matrix[bottom][i]);
+        for(let i=bottom;i>top;i--){
+            result.push(matrix[i][left]);
+        }
+        top++;
+        bottom--;
+        left++;
+        right--;
+    }
+    //剩下一行和一列的情况
+    if(top==bottom){
+        for(let i=left;i<=right;i++){ //注意这里是小于等于，因为如果中间只有一个元素的话，左右上下都相等
+            result.push(matrix[top][i]);
+        }
+    }
+    else if(left==right){ //注意要用else if，如果只有一个元素的时候只做一个条件判断就可以了，不然会多放
+        for(let i=top;i<=bottom;i++){ //注意这里是小于等于，因为如果中间只有一个元素的话，左右上下都相等
+            result.push(matrix[i][left]);
+        }
+    }
+    return result;
 };
 // @lc code=end
 
